0fc45a69019e3a7f4403863023a7abf9bf3d6c87
XiangchenShen <xiangchenshen@163.com>
2024-10-16 20:23:05 +0800

    add stateful testing 1.0

--- kea/Bundle.py
+++ kea/Bundle.py
@@ -1,8 +1,40 @@
+import random
+import string
+
+# from kea.main import *
+from hypothesis import strategies as st
+
 class Bundle():
-    def __init__(self, name: str, draw_references: bool = True) -> None:
-        self.name = name
-        self.draw_references = draw_references
+    def __init__(self, type: str = None) -> None:
+        self.type = type
         self.data = []
 
+    def add_data(self, name = None):
+        if name is None:
+            name = st.text(alphabet=string.ascii_letters,min_size=1, max_size=10).example()
+        self.data.append(name)
+        return name
 
+    def del_data(self, name = None):
+        if name is None:
+            random_item = random.choice(self.data)
+            name = random_item
+        self.data.remove(name)
+        return name
+
+    def update_data(self, name = None, value = None):
+        if value is None:
+            value = st.text(alphabet=string.ascii_letters,min_size=1, max_size=10).example()
+        if name is None:
+            random_item = random.choice(self.data)
+            name = random_item
+        try:
+            self.data.remove(name)
+            self.data.append(value)
+        except KeyError:
+            print(f"'{name}' is not a object of Bundle.")
+        return name, value
+
+    def get_all_data(self):
+        return self.data
 

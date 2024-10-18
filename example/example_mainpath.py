3c3b865de67c05383e3b2b29c2b03f3967a245de
XiangchenShen <xiangchenshen@163.com>
2024-10-18 21:13:50 +0800

    update main_path

--- example/example_mainpath.py
+++ example/example_mainpath.py
@@ -9,25 +9,32 @@ class Test(Kea):
             d(resourceId="it.feio.android.omninotes.alpha:id/next").click()
         d(resourceId="it.feio.android.omninotes.alpha:id/done").click()
 
-    @precondition(lambda self: d(resourceId="it.feio.android.omninotes:id/menu_tag").exists() and
-                               "#" in d(resourceId="it.feio.android.omninotes:id/detail_content").info["text"]
+    @main_path()
+    def test_main(self):
+        d(resourceId="it.feio.android.omninotes.alpha:id/fab_expand_menu_button").long_click()
+        d(resourceId="it.feio.android.omninotes.alpha:id/detail_content").click()
+        d.send_keys("read a book #Tag1", clear=True)
+        d(description="drawer open").click()
+
+    @precondition(lambda self: d(resourceId="it.feio.android.omninotes.alpha:id/menu_tag").exists() and
+                               "#" in d(resourceId="it.feio.android.omninotes.alpha:id/detail_content").info["text"]
                   )
     @rule()
     def rule_remove_tag_from_note_shouldnot_affect_content(self):
         # get the text from the note's content
-        origin_content = d(resourceId="it.feio.android.omninotes:id/detail_content").info["text"]
+        origin_content = d(resourceId="it.feio.android.omninotes.alpha:id/detail_content").info["text"]
         # click to open the tag list
-        d(resourceId="it.feio.android.omninotes:id/menu_tag").click()
+        d(resourceId="it.feio.android.omninotes.alpha:id/menu_tag").click()
         # select a tag to remove
         selected_tag = random.choice(d(className="android.widget.CheckBox", checked=True))
         select_tag_name = "#" + \
-                          selected_tag.right(resourceId="it.feio.android.omninotes:id/md_title").info["text"].split(
+                          selected_tag.right(resourceId="it.feio.android.omninotes.alpha:id/md_title").info["text"].split(
                               " ")[0]
         selected_tag.click()
         # click to uncheck the selected tag
         d(text="OK").click()
         # get the updated content after removing the tag
-        new_content = d(resourceId="it.feio.android.omninotes:id/detail_content").info["text"].strip().replace(
+        new_content = d(resourceId="it.feio.android.omninotes.alpha:id/detail_content").info["text"].strip().replace(
             "Content", "")
         # get the expected content after removing the tag
         origin_content_exlude_tag = origin_content.replace(select_tag_name, "").strip()

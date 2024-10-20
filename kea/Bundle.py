60bfbbb910f6ee9921239426fe941a7a9ae51cb7
XiangchenShen <xiangchenshen@163.com>
2024-10-20 17:30:16 +0800

    simplify function name

--- kea/Bundle.py
+++ kea/Bundle.py
@@ -8,36 +8,36 @@ class Bundle():
         self.type = type
         self.data = []
 
-    def add_data(self, name = None):
-        if name is None:
+    def add(self, test = None):
+        if test is None:
             raise ValueError("the value of " + self.type + " cannot be None")
-        self.data.append(name)
+        self.data.append(test)
         print(self.type, self.data)
 
-    def del_data(self, name = None):
-        if name is None:
+    def delete(self, test = None):
+        if test is None:
             raise ValueError("the value of " + self.type + " cannot be None")
-        self.data.remove(name)
+        self.data.remove(test)
         print(self.type, self.data)
 
-    def update_data(self, name = None, value = None):
+    def update(self, test = None, value = None):
         if value is None:
             raise ValueError("the new name of " + self.type + " cannot be None")
-        if name is None:
+        if test is None:
             raise ValueError("the old name of " + self.type + " cannot be None")
         try:
-            self.data.remove(name)
+            self.data.remove(test)
             self.data.append(value)
         except KeyError:
-            print(f"'{name}' is not a object of Bundle.")
+            print(f"'{test}' is not a object of Bundle.")
         print(self.type, self.data)
 
     def get_all_data(self):
         return self.data
 
-    def get_random_name(self, name_len = 10):
-        name = st.text(alphabet=string.ascii_letters, min_size=1, max_size=name_len).example()
-        return name
+    def get_random_test(self, test_len = 10):
+        test = st.text(alphabet=string.ascii_letters, min_size=1, max_size=test_len).example()
+        return test
 
     def get_random_data(self):
         random_item = random.choice(self.data)

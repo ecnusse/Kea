41dcd23ff5dc39df8d944e4d9a2b17d6f3d6afae
XixianLiang <51265902107@stu.ecnu.edu.cn>
2024-12-28 16:06:57 +0800

    update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,7 +2,6 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath("../../"))
-# sys.path.insert(0, os.path.abspath("../kea"))
 # Configuration file for the Sphinx documentation builder.
 
 # -- Project information

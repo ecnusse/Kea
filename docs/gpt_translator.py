215c2328af980f11a5a3af4b58389051847675a5
XixianLiang <51265902107@stu.ecnu.edu.cn>
2025-01-01 22:56:16 +0800

    update gpt script

--- docs/gpt_translator.py
+++ docs/gpt_translator.py
@@ -1,18 +1,21 @@
 import os
-from gpt_translator_utils import OpenaiTranslator, POT_TRANSLATION_PROMPT
+from gpt_translator_utils import OpenaiTranslator, POT_TRANSLATION_PROMPT, get_not_translated_files
+
 
 if __name__ == "__main__":
     # 从环境变量中读取 API 密钥
     api_key = os.getenv("OPENAI_API_KEY")
     translater = OpenaiTranslator(api_key=api_key, prompt=POT_TRANSLATION_PROMPT)
     
-    cur_dir = os.path.split(os.path.abspath(__file__))[0]
-    locales_path = os.path.join(cur_dir, "source", "locales")
-    print(f"walking dir {locales_path} for po files")
-    for root, dirs, files in os.walk(locales_path):
-        for file in files:
-            if file.endswith('.po'):
-                # 获取完整的文件路径
-                file_path = os.path.join(root, file)
-                print(file_path)
-                translater.translate_file(file_path)
+    docs_dir = os.path.split(os.path.abspath(__file__))[0]
+    os.chdir(docs_dir)
+    
+    untranslated_files = get_not_translated_files()
+    
+    print(f"found {len(untranslated_files)} untranslated files.")
+    for untranslated_file in untranslated_files:
+        print(untranslated_file)
+    
+    for untranslated_file in untranslated_files:
+        abs_path = os.path.join(docs_dir, untranslated_file)
+        translater.translate_file(abs_path)
